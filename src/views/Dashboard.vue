6d0a01c746f483ec79968a47714ba90611438528
Leonel Lescano <108104969+leonel-05@users.noreply.github.com>
2024-12-02 20:02:53 -0300

    Creamos la vista Login y la Dashboard incompleta

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Navbar />
+    <h1>Hola, {{ $store.state.user }} 👋</h1>
+    <section>
+      <h2>Tu Balance</h2>
+      <div class="balances">
+        <BalanceCard
+          v-for="(balance, coin) in $store.state.balances"
+          :key="coin"
+          :coinName="coin"
+          :balance="balance"
+          :price="$store.state.prices[coin]"
+        />
+      </div>
+    </section>
+    <section>
+      <TransitionList :transaction="$store.state.transactions" />
+    </section>
+  </div>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="login">
+    <h1>MySafeWallet</h1>
+    <input
+      v-model="username"
+      placeholder="Ingrese su usuario"
+      @keypress.enter="login"
+    />
+    <button @click="login">Ingresar</button>
+    <p v-if="error" class="error">{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { username: "", error: "" };
+  },
+  methods: {
+    login() {
+      if (this.username.trim()) {
+        this.$store.commit("setUser", this.username);
+        this.$router.push("/dashboard");
+      } else {
+        this.error = "El nombre de usuario es obligatorio. ";
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+#login {
+  text-align: center;
+  margin-top: 10%;
+}
+input,
+button {
+  margin: 10px;
+}
+.error {
+  color: red;
+}
+</style>
